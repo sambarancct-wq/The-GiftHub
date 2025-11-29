058aaaf2a0a3d0ce2e1584cfe2a9736813552d6a
Sambaran Banerjee <sambaran.cct@gmail.com>
2025-11-29 20:36:51 +0530

    Update ServerApplication.java

--- server/src/main/java/com/giftregistry/server/ServerApplication.java
+++ server/src/main/java/com/giftregistry/server/ServerApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServerApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(ServerApplication.class, args);
 	}
